5cb9b1f7219dfecb1c0e4bce0390971b8c006d56
igmansilla <88914404+igmansilla@users.noreply.github.com>
2024-10-17 16:42:28 -0300

    add spring app a proveedor

---
+++ proveedor/src/main/java/proveedor/models/CatalogoProducto.java
@@ -0,0 +1,24 @@
+package proveedor.models;
+
+import lombok.Data;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "catalogo_producto")
+@Data
+public class CatalogoProducto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "catalogo_id", referencedColumnName = "id", nullable = false)
+    private Catalogo catalogo;
+
+    @ManyToOne
+    @JoinColumn(name = "producto_id", referencedColumnName = "id", nullable = false)
+    private Producto producto;
+}
